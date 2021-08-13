81876c8b23b1865566fdc2eb10946efe2a654cc7
Danya Generalov <danya@danya02.ru>
2021-08-13 19:39:30 +0100

    Adding error names, links and list page.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask, abort, make_response
+from flask import Flask, abort, make_response, render_template
+import yaml
+import traceback
 
 app = Flask(__name__)
+
+def get_errors():
+    with open('/ERRORS.yml') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+
+    endpoints = []
+    for item in data:
+        if isinstance(item, str):
+            code = int(item[:3])
+            endpoints.append( (item, f'/{code}') )
+        else:
+            endpoints.append( (item['name'], item['url']) )
+    return endpoints
 
 def verbatim(*args, **kwargs):
     response = make_response(*args, **kwargs)
@@ -13,11 +28,21 @@ def body_and_error(title, body=None):
     response.headers['X-Errorpage-Title'] = title
     if body:
         response.headers['X-Errorpage-Body'] = body
-    return response
+    return verbatim(response)
+
+@app.route('/')
+def index():
+    try:
+        errors = get_errors()
+    except:
+        err = traceback.format_exc()
+        return body_and_error("Error list misconfigured!", "<p>You can still test errors by appending <code>/(error_code)</code> to this address.</p><p>Error code:</p><p><code>" + err.replace('\n', '<br>') + "</code></p>")
+
+    return render_template('index.html', errors=errors)
 
 
 @app.route('/<int:error_id>')
 def error(error_id):
     if error_id < 400 or error_id >= 600:
-        return verbatim(body_and_error("Error codes must be between 400 and 599, inclusive.")), 404
+        return body_and_error("Error codes must be between 400 and 599, inclusive."), 404
     return 'error '+str(error_id), error_id
